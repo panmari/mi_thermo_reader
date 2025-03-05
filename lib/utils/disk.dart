c784cdb57069a53749b1bf9b76795886c56806be
panmari <smmuzi@gmail.com>
2025-03-05 13:04:14 +0100

    Done with code for serializing proto messages to a file.

--- lib/utils/disk.dart
+++ lib/utils/disk.dart
@@ -2,13 +2,18 @@ import 'dart:io';
 
 import 'package:mi_thermo_reader/utils/sensor_entry.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:protobuf/protobuf.dart';
 
 class DiskOperations {
-  static Future<bool> save(List<SensorEntry> entries) async {
+
+  // Writes the given sensor entries to a file.
+  static Future<File> save(String deviceIdentifier, List<SensorEntry> entries) async {
     final directory = await getApplicationDocumentsDirectory();
-    final file = File('$directory/entries.txt');
-    // TODO: Also include device id
-    // TODO: Write content to file (in proto format).
-    return true;
+    final file = File('$directory/$deviceIdentifier/sensor_entries.pb');
+    final buffer = CodedBufferWriter();
+    for (SensorEntry e in entries) {
+      e.toProto().writeToCodedBufferWriter(buffer);
+    }
+    return file.writeAsBytes(buffer.toBuffer());
   }
 }
