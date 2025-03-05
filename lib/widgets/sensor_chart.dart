b3a479895bd11a2c1eda55305f6f7b7ea2f8223b
panmari <smmuzi@gmail.com>
2025-03-05 21:35:30 +0100

    Some more tweaks to sensor chart.

--- lib/widgets/sensor_chart.dart
+++ lib/widgets/sensor_chart.dart
@@ -4,24 +4,27 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 import '../utils/sensor_entry.dart';
 
 class SensorChart extends StatelessWidget {
-
   final List<SensorEntry> sensorEntries;
 
   const SensorChart({super.key, required this.sensorEntries});
 
   @override
   Widget build(BuildContext context) {
-                   return SfCartesianChart(
-                        primaryXAxis: DateTimeAxis(),
-                        series: <CartesianSeries>[
-                            // Renders line chart
-                            LineSeries<SensorEntry, DateTime>(
-                                dataSource: sensorEntries,
-                                xValueMapper: (SensorEntry e, _) => e.timestamp,
-                                yValueMapper: (SensorEntry e, _) => e.temperature,
-                            )
-                        ]
-                    );
+    return SfCartesianChart(
+      title: ChartTitle(text: 'Temperature'),
+      tooltipBehavior: TooltipBehavior(enable: true),
+      primaryXAxis: DateTimeAxis(),
+      primaryYAxis: const NumericAxis(
+        minimum: 20, // TODO(panmari): Compute.
+      ),
+      series: <CartesianSeries>[
+        // Renders line chart
+        LineSeries<SensorEntry, DateTime>(
+          dataSource: sensorEntries,
+          xValueMapper: (SensorEntry e, _) => e.timestamp,
+          yValueMapper: (SensorEntry e, _) => e.temperature,
+        ),
+      ],
+    );
   }
-  
 }
