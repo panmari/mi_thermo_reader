e8db2dcfb368c189cdf04ee31c1cf9e90102e96e
panmari <smmuzi@gmail.com>
2025-03-06 08:17:42 +0100

    Done with writing/loading sensor history.

--- lib/utils/disk.dart
+++ lib/utils/disk.dart
@@ -1,26 +1,33 @@
 import 'dart:io';
 
+import 'package:mi_thermo_reader/src/proto/model.pb.dart';
 import 'package:mi_thermo_reader/utils/sensor_entry.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:protobuf/protobuf.dart';
 
 class DiskOperations {
-
   static Future<String> _filepath(String deviceIdentifier) {
-    return getApplicationDocumentsDirectory().then((d) =>  '$d/$deviceIdentifier/sensor_entries.pb');
+    return getApplicationDocumentsDirectory().then(
+      (d) => '$d/$deviceIdentifier/sensor_entries.pb',
+    );
   }
+
   // Writes the given sensor entries to a file.
-  static Future<File> save(String deviceIdentifier, List<SensorEntry> entries) async {
+  static Future<File> save(
+    String deviceIdentifier,
+    List<SensorEntry> entries,
+  ) async {
     final filePath = await _filepath(deviceIdentifier);
     final file = File(filePath);
-    return file.writeAsBytes(SensorHistory(sensorEntries: entries).toProto());
+    return file.writeAsBytes(
+      SensorHistory(sensorEntries: entries).toProto().writeToBuffer(),
+    );
   }
 
   static Future<List<SensorEntry>> load(String deviceIdentifier) async {
     final filePath = await _filepath(deviceIdentifier);
-      
-    final bytes = await File(filePath).readAsBytesSync();
-    final reader = CodedBufferReader(bytes);
-   return [];
+    final bytes = File(filePath).readAsBytesSync();
+
+    return GSensorHistory.fromBuffer(bytes).toSensorHistory().sensorEntries;
   }
 }
