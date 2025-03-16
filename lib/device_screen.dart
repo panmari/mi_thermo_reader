5fa21fd7047a55a1393c9765d5f3a365c7d01141
panmari <smmuzi@gmail.com>
2025-03-16 22:05:35 +0100

    Improving code health of parsing code.

--- lib/device_screen.dart
+++ lib/device_screen.dart
@@ -149,7 +149,9 @@ class _DeviceScreenState extends State<DeviceScreen> {
       });
     }
 
-    _valueSubscription = _memoCharacteristic!.onValueReceived.listen(processReceivedData);
+    _valueSubscription = _memoCharacteristic!.onValueReceived.listen(
+      processReceivedData,
+    );
     widget.device.cancelWhenDisconnected(_valueSubscription!);
 
     try {
@@ -169,14 +171,16 @@ class _DeviceScreenState extends State<DeviceScreen> {
     try {
       // Send command to read memory measures.
       // See https://github.com/pvvx/ATC_MiThermometer?tab=readme-ov-file#primary-service-uuid-0x1f10-characteristic-uuid-0x1f1f
-      final numMemo = 5000;
-      final start = 0; // TODO(panmari): Figure out  how this affects getMemo.
+      // The two parameters are:
+      final lastNumMemo =
+          5000; // How many records to fetch, starting with the most recent.
+      final skipNumMemo = 0; // How many records to skip from the start.
       _memoCharacteristic!.write([
         0x35,
-        numMemo & 0xff,
-        (numMemo >> 8) & 0xff,
-        start & 0xff,
-        (start >> 8) & 0xff,
+        lastNumMemo & 0xff,
+        (lastNumMemo >> 8) & 0xff,
+        skipNumMemo & 0xff,
+        (skipNumMemo >> 8) & 0xff,
       ], withoutResponse: true);
     } catch (e) {
       _statusUpdates.add("Start get memo failed: $e");
@@ -186,61 +190,62 @@ class _DeviceScreenState extends State<DeviceScreen> {
   }
 
   void processReceivedData(List<int> values) {
-      if (values.isEmpty) {
-        return;
-      }
-      final data = ByteData.view(Uint8List.fromList(values).buffer);
-      final blkid = data.getInt8(0);
-      if (blkid == 0x35) {
-        if (data.lengthInBytes >= 13) {
-          // Got an entry from memory. Convert it to a SensorEntry.
-          _sensorEntries.add(SensorEntry.parse(data));
-          return;
-        }
-        if (data.lengthInBytes >= 3) {
-          // They are sent in reverse chronological order, and might be received out of order.
-          // Plus there might be retries. Be very defensive about keeping each value only once.
-          _sensorEntries.sort((a, b) => a.timestamp.compareTo(b.timestamp));
-          final alreadyPresent = Set<DateTime>();
-          _sensorEntries.retainWhere((e) => alreadyPresent.add(e.timestamp));
-          if (mounted) {
-            setState(() {
-              _statusUpdates.add(
-                'Done with reading. Got ${_sensorEntries.length} samples',
-              );
-            });
-          }
-          _preferences.then((p) {
-            final encodedEntries =
-                SensorHistory(
-                  sensorEntries: _sensorEntries,
-                ).toBase64ProtoString();
-            p.setString(widget.cacheKeyName, encodedEntries);
-            setState(() {
-              _isUpdatingData = false;
-              _statusUpdates.add(
-                'Saved ${_sensorEntries.length} entries to preferences.',
-              );
-            });
-          });
-          // Original code sets dev time here.
-          // Disconnect here, we're done. Not using await, since the listener can't be async.
-          widget.device.disconnect().then((_) {
-            setState(() {
-              _statusUpdates.add("Disconnected");
-            });
-          });
-          return;
-        }
-        if (data.lengthInBytes == 2) {
-          final numSamples = data.getUint16(1, Endian.little);
-          setState(() {
-            _statusUpdates.add('Number of samples in memory: $numSamples');
-          });
-          return;
-        }
-      }
+    if (values.isEmpty) {
+      return;
     }
+    final data = ByteData.view(Uint8List.fromList(values).buffer);
+    final blkid = data.getInt8(0);
+    if (blkid != 0x35) {
+      log("data with unexpected blkid $blkid: $data");
+      return;
+    }
+    if (data.lengthInBytes >= 13) {
+      // Got an entry from memory. Convert it to a SensorEntry.
+      _sensorEntries.add(SensorEntry.parse(data));
+      return;
+    }
+    if (data.lengthInBytes >= 3) {
+      // They are sent in reverse chronological order, and might be received out of order.
+      // Plus there might be retries. Be very defensive about keeping each value only once.
+      _sensorEntries.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+      final alreadyPresent = Set<DateTime>();
+      _sensorEntries.retainWhere((e) => alreadyPresent.add(e.timestamp));
+      if (mounted) {
+        setState(() {
+          _statusUpdates.add(
+            'Done with reading. Got ${_sensorEntries.length} samples',
+          );
+        });
+      }
+      _preferences.then((p) {
+        final encodedEntries =
+            SensorHistory(sensorEntries: _sensorEntries).toBase64ProtoString();
+        p.setString(widget.cacheKeyName, encodedEntries);
+        setState(() {
+          _isUpdatingData = false;
+          _statusUpdates.add(
+            'Saved ${_sensorEntries.length} entries to preferences.',
+          );
+        });
+      });
+      // Original code sets dev time here.
+      // Disconnect here, we're done. Not using await, since the listener can't be async.
+      widget.device.disconnect().then((_) {
+        setState(() {
+          _statusUpdates.add("Disconnected");
+        });
+      });
+      return;
+    }
+    if (data.lengthInBytes == 2) {
+      // TODO(panmari): This message seems pointless. Seems to be mostly 0 if received.
+      final numSamples = data.getUint16(1, Endian.little);
+      setState(() {
+        _statusUpdates.add('Number of samples in memory: $numSamples');
+      });
+      return;
+    }
+  }
 
   List<SensorEntry> _filteredSensorEntries() {
     if (_sensorEntries.isEmpty) {
