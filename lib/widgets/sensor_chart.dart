fea453d3da88cf78d71aac5d136f325dcbf5442c
panmari <smmuzi@gmail.com>
2025-03-17 16:44:03 +0100

    Moving do different graphing package.

--- lib/widgets/sensor_chart.dart
+++ lib/widgets/sensor_chart.dart
@@ -1,31 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-
-import '../utils/sensor_entry.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:intl/intl.dart';
+import 'package:mi_thermo_reader/utils/sensor_entry.dart';
 
 class SensorChart extends StatelessWidget {
   final List<SensorEntry> sensorEntries;
 
   const SensorChart({super.key, required this.sensorEntries});
 
+  String _formatDate(double value) {
+    final dateTime = DateTime.fromMillisecondsSinceEpoch(value.toInt());
+    return DateFormat('MMM d').format(dateTime); // Format as "Month Day"
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SfCartesianChart(
-      title: ChartTitle(text: 'Temperature'),
-      tooltipBehavior: TooltipBehavior(enable: true),
-      primaryXAxis: DateTimeAxis(),
-      primaryYAxis: const NumericAxis(
-        name: 'Temperature',
-        minimum: 20, // TODO(panmari): Compute.
-      ),
-      series: <CartesianSeries>[
-        // Renders line chart
-        LineSeries<SensorEntry, DateTime>(
-          dataSource: sensorEntries,
-          xValueMapper: (SensorEntry e, _) => e.timestamp,
-          yValueMapper: (SensorEntry e, _) => e.temperature,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: AspectRatio(
+        aspectRatio: 1.7,
+        child: LineChart(
+          LineChartData(
+            gridData: FlGridData(show: true),
+            titlesData: FlTitlesData(
+              show: true,
+              bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  getTitlesWidget:
+                      (value, meta) => Text(
+                        _formatDate(value),
+                        style: const TextStyle(fontSize: 10),
+                      ),
+                  // reservedSize: 22,
+                  // interval:
+                  //     _chartData.length > 1
+                  //         ? (_chartData.last.x - _chartData.first.x) /
+                  //             (_chartData.length - 1)
+                  //         : 1, // Ensure all labels show
+                ),
+              ),
+            ),
+            minX:
+                sensorEntries.first.timestamp.millisecondsSinceEpoch.toDouble(),
+            maxX:
+                sensorEntries.last.timestamp.millisecondsSinceEpoch.toDouble(),
+            minY: 20,
+            maxY: 25,
+            lineBarsData: [
+              LineChartBarData(
+                spots:
+                    sensorEntries
+                        .map(
+                          (s) => FlSpot(
+                            s.timestamp.millisecondsSinceEpoch.toDouble(),
+                            s.temperature,
+                          ),
+                        )
+                        .toList(),
+              ),
+            ],
+          ),
         ),
-      ],
+      ),
     );
   }
 }
