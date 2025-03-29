75316a06e2caa779942ad996cfc7b6f95b35d931
panmari <smmuzi@gmail.com>
2025-03-29 19:05:30 +0100

    Add widget test for system device tile.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,10 +3,11 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mi_thermo_reader/widgets/scan_result_tile.dart';
+import 'package:mi_thermo_reader/widgets/system_device_tile.dart';
 
 void main() {
   group('ScanTile', () {
-    testWidgets('Has ID', (tester) async {
+    testWidgets('Has ID and rssi', (tester) async {
       final scanTile = ScanResultTile(
         result: ScanResult(
           device: BluetoothDevice.fromId('06:E5:28:3B:FD:E0'),
@@ -23,6 +24,24 @@ void main() {
           timeStamp: DateTime.now(),
         ),
         onTap: () => 'nothing',
+      );
+
+      // Wrap your ListTile with MaterialApp or another suitable parent widget.
+      await tester.pumpWidget(MaterialApp(home: Scaffold(body: scanTile)));
+
+      final idFinder = find.text('06:E5:28:3B:FD:E0');
+      final rssiFinder = find.text('31');
+
+      expect(idFinder, findsOneWidget);
+      expect(rssiFinder, findsOneWidget);
+    });
+  });
+
+  group('DeviceTile', () {
+    testWidgets('Has ID', (tester) async {
+      final scanTile = SystemDeviceTile(
+        device: BluetoothDevice.fromId('06:E5:28:3B:FD:E0'),
+        onOpen: () => 'nothing',
       );
 
       // Wrap your ListTile with MaterialApp or another suitable parent widget.
