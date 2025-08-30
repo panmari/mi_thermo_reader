a6607aa99244a62db5d0726dbb73e0cbbee6e326
panmari <smmuzi@gmail.com>
2025-08-30 08:35:37 +0200

    Add option to export data as CSV

--- lib/widgets/popup_menu.dart
+++ lib/widgets/popup_menu.dart
@@ -1,21 +1,26 @@
 import 'dart:developer' as developer;
 
+import 'package:file_saver/file_saver.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:mi_thermo_reader/utils/sensor_entry.dart';
+import 'package:mi_thermo_reader/widgets/about_dialog_content.dart';
 
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:app_review_plus/app_review_plus.dart';
 
-enum Selection { about, rate, fixTime }
+enum Selection { about, rate, fixTime, export }
 
 /// For retrieving PackageInfo async, the actual PopupMenu is wrapped
 /// in this stateful widget.
 class PopupMenu extends StatefulWidget {
   final Function? getAndFixTime;
+  final List<SensorEntry>? sensorEntries;  
 
-  const PopupMenu({super.key, this.getAndFixTime});
+  const PopupMenu({super.key, this.getAndFixTime, this.sensorEntries});
 
   @override
   State<PopupMenu> createState() => _PopupMenuState();
@@ -32,13 +37,25 @@ class _PopupMenuState extends State<PopupMenu> {
     });
   }
 
+  String _sensorEntriesToCsv(List<SensorEntry> entries) {
+    final buffer = StringBuffer();
+    // Header
+    buffer.writeln('timestamp,temperature,humidity,voltageBattery');
+    // Data
+    for (final entry in entries) {
+      buffer.writeln(
+          '${entry.timestamp.toIso8601String()},${entry.temperature.toStringAsFixed(2)},${entry.humidity.toStringAsFixed(2)},${entry.voltageBattery}');
+    }
+    return buffer.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<PackageInfo>(
       future: _packageInfo,
       builder: (context, snapshot) {
-        return PopupMenuButton<Selection>(
-          onSelected: (Selection result) {
+        return PopupMenuButton<Selection>(          
+          onSelected: (Selection result) async {
             switch (result) {
               case Selection.about:
                 showAboutDialog(
@@ -112,6 +129,23 @@ class _PopupMenuState extends State<PopupMenu> {
               case Selection.fixTime:
                 widget.getAndFixTime!();
                 break;
+              case Selection.export:
+                if (widget.sensorEntries != null &&
+                    widget.sensorEntries!.isNotEmpty) {
+                  final csvData = _sensorEntriesToCsv(widget.sensorEntries!);
+                  final now = DateTime.now();
+                  final formatter = DateFormat('yyyy-MM-dd');
+                  final formattedDate = formatter.format(now);
+                  final filename = 'mi_thermo_reader_export_$formattedDate';
+
+                  await FileSaver.instance.saveFile(
+                    name: filename,
+                    bytes: Uint8List.fromList(utf8.encode(csvData)),
+                    ext: 'csv',
+                    mimeType: MimeType.csv,
+                  );
+                }
+                break;
             }
           },
           itemBuilder: (BuildContext context) => _menuItemBuilder(context),
@@ -126,6 +160,12 @@ class _PopupMenuState extends State<PopupMenu> {
         PopupMenuItem<Selection>(
           value: Selection.fixTime,
           child: Text('Adjust time'),
+        ),
+      if (widget.sensorEntries != null &&
+          widget.sensorEntries!.isNotEmpty)
+        const PopupMenuItem<Selection>(
+          value: Selection.export,
+          child: Text('Export to CSV'),
         ),
       if (!kIsWeb)
         PopupMenuItem<Selection>(
