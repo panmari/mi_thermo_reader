22b80c5e44c5db380fc73878f058eb7c2f2d95a2
panmari <smmuzi@gmail.com>
2025-12-20 20:55:59 +0100

    Add support for pvvx advertisements.
    Parser written by @DienoX.

--- lib/services/bluetooth_advertisement_parsers/thermometer_advertisement.dart
+++ lib/services/bluetooth_advertisement_parsers/thermometer_advertisement.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:mi_thermo_reader/services/bluetooth_advertisement_parsers/bt_home_v2_parser.dart';
+import 'package:mi_thermo_reader/services/bluetooth_advertisement_parsers/pvvx_parser.dart';
 import 'package:mi_thermo_reader/services/bluetooth_constants.dart';
 
 class NoAdvertisementDataFound implements Exception {
@@ -28,6 +29,18 @@ class ThermometerAdvertisement {
         temperature: parsed[ObjectId.temperature] as double? ?? double.nan,
         humidity: parsed[ObjectId.humidity] as double? ?? double.nan,
         batteryLevel: parsed[ObjectId.battery] as int? ?? -1,
+      );
+    }
+    final pvvxValues =
+        advertisementData.serviceData[BluetoothConstants
+            .pvvxAdvertisingFormatGuid];
+    if (pvvxValues != null) {
+      final parsed = PvvxParser.parse(pvvxValues);
+
+      return ThermometerAdvertisement(
+        temperature: parsed.temperature,
+        humidity: parsed.humidity,
+        batteryLevel: parsed.batteryLevel,
       );
     }
 
