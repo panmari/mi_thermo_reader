6d8c8710c0cd620a1f4d8d147a14bfb7586b6f41
panmari <smmuzi@gmail.com>
2025-12-28 20:03:39 +0100

    Cover temperature/humidity on homepage in test.

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mi_thermo_reader/home_page.dart';
 import 'package:mi_thermo_reader/main.dart';
+import 'package:mi_thermo_reader/services/bluetooth_constants.dart';
 import 'package:mi_thermo_reader/utils/known_device.dart';
 import 'package:mockito/mockito.dart';
 
@@ -14,6 +15,45 @@ base class _FakeFlutterBluePlus extends FlutterBluePlusPlatform {
   Stream<BmBluetoothAdapterState> get onAdapterStateChanged {
     return Stream.value(
       BmBluetoothAdapterState(adapterState: BmAdapterStateEnum.on),
+    );
+  }
+
+  @override
+  Stream<BmScanResponse> get onScanResponse {
+    return Stream.value(
+      BmScanResponse(
+        advertisements: [
+          BmScanAdvertisement(
+            remoteId: DeviceIdentifier('1x:2y'),
+            platformName: 'ignored',
+            advName: 'ignored',
+            connectable: true,
+            txPowerLevel: null,
+            appearance: null,
+            manufacturerData: {},
+            serviceData: {
+              BluetoothConstants.btHomeReversedGuid: [
+                64,
+                0,
+                167,
+                1,
+                92,
+                2,
+                29,
+                9,
+                3,
+                255,
+                14,
+              ],
+            },
+            serviceUuids: [],
+            rssi: 0,
+          ),
+        ],
+        success: true,
+        errorCode: 0,
+        errorString: "",
+      ),
     );
   }
 }
@@ -69,6 +109,7 @@ void main() {
     );
     // Known device is displayed.
     expect(find.text('some_device_adv'), findsOneWidget);
+    expect(find.text('Sensor reading not available'), findsOneWidget);
     expect(
       find.text('Bluetooth is unknown, cannot add devices'),
       findsOneWidget,
@@ -79,6 +120,7 @@ void main() {
     await tester.pump();
 
     expect(find.text('some_device_adv'), findsOneWidget);
+    expect(find.text('Temperature: 23.33°C, Humidity: 38.39%'), findsOneWidget);
     expect(find.byIcon(Icons.add), findsOneWidget);
     expect(find.text('Add device'), findsOneWidget);
   });
